dc82c638367acaf64d8ace211973f2788f954f01
Ben Clothier <ben@exabytegames.com>
2022-05-12 19:29:45 +0100

    Remove commented-out includes

--- src/Shader.h
+++ src/Shader.h
@@ -5,8 +5,6 @@
 #define SHADER_H
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
-//#include <glm/glm.hpp>
-//#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <fstream>
 #include <sstream>
