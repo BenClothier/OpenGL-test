d0bb88ab8e28784d97a5c4c3bb8741c7e749cd7e
Ben Clothier <ben.clothier8@icloud.com>
2022-06-04 11:17:10 +0100

    Add notion of entity-component signature

--- src/core/Types.hpp
+++ src/core/Types.hpp
@@ -1,9 +1,17 @@
 #pragma once
 #include <cstdint>
+#include <bitset>
 
-// Entity Component System
-using Entity = std::uint32_t; // Each entity is defined by a simple ID
+// ==== Entity Component System ==== //
+
+// Each entity is defined by a simple ID
+using Entity = std::uint32_t;
 const Entity MAX_ENTITIES = 5000;
 
-using ComponentType = std::uint8_t; // Each component has a simple ID assigned to it
-const ComponentType MAX_COMPONENTS = 32;
+ // Each component has a simple ID assigned to it
+using ComponentType = std::uint8_t;
+const ComponentType MAX_COMPONENT_TYPES = 32;
+
+// Each entity has a signature representing which components it has
+// (e.g. An entity with signature 0b101, would have components 1 and 3)
+using Signature = std::bitset<MAX_COMPONENT_TYPES>;
