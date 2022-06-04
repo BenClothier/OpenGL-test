1ae2bbe0cbdd78b98a60feeccacafc5951119b21
Ben Clothier <ben.clothier8@icloud.com>
2022-06-04 11:07:18 +0100

    Rename files and folders

--- src/components/Transform.hpp
+++ src/components/Transform.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "math/Vec3.hpp"
+#include "Math/Vec3.hpp"
 
 struct Transform
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include "core/shader.h"
+#include "Core/Shader.hpp"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
