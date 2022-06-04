10367bbe28621d68bd6b4f3b4b3d8501a2f39f21
Ben Clothier <ben.clothier8@icloud.com>
2022-06-04 12:25:48 +0100

    Add system manager

---
+++ src/core/SystemManager.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <cassert>
+#include <memory>
+#include <unordered_map>
+
+#include "System.hpp"
+#include "Types.hpp"
+
+class SystemManager
+{
+public:
+	template<typename T>
+	std::shared_ptr<T> RegisterSystem()
+	{
+		const char* typeName = typeid(T).name();
+
+		assert(systems.find(typeName) == systems.end() && "Registering system more than once.");
+
+		// Create a pointer to the system and return it so it can be used externally
+		auto system = std::make_shared<T>();
+		systems.insert({typeName, system});
+		return system;
+	}
+
+	template<typename T>
+	void SetSignature(Signature signature)
+	{
+		const char* typeName = typeid(T).name();
+
+		assert(systems.find(typeName) != systems.end() && "System used before registered.");
+
+		// Set the signature for this system
+		signatures.insert({typeName, signature});
+	}
+
+	void EntityDestroyed(Entity entity)
+	{
+		// Erase a destroyed entity from all system entity-sets
+		for (auto const& pair : systems)
+		{
+			auto const& system = pair.second;
+			system -> entities.erase(entity);
+		}
+	}
+
+	void EntitySignatureChanged(Entity entity, Signature entitySignature)
+	{
+		// Notify each system that an entity's signature changed
+		for (auto const& pair : systems)
+		{
+			auto const& type = pair.first;
+			auto const& system = pair.second;
+			auto const& systemSignature = signatures[type];
+
+			// Entity signature matches system signature - insert into set
+			if ((entitySignature & systemSignature) == systemSignature)
+			{
+				system -> entities.insert(entity);
+			}
+
+			// Entity signature does not match system signature - erase from set
+			else
+			{
+				system -> entities.erase(entity);
+			}
+		}
+	}
+
+private:
+	// Map from system type string pointer to a signature
+	std::unordered_map<const char*, Signature> signatures{};
+
+	// Map from system type string pointer to a system pointer
+	std::unordered_map<const char*, std::shared_ptr<System>> systems{};
+};
