4bccfa680da5a815f22563a3051e0ebc736cfc7c
Ben Clothier <ben.clothier8@icloud.com>
2022-06-04 12:27:23 +0100

    Rename symbols in EntityManager.hpp

--- src/core/EntityManager.hpp
+++ src/core/EntityManager.hpp
@@ -6,18 +6,18 @@ public:
 		// Population queue with all possible entity IDs
 		for (Entity entity = 0; entity < MAX_ENTITIES; ++entity)
 		{
-			mAvailableEntities.push(entity);
+			availableEntities.push(entity);
 		}
 	}
 
 	Entity CreateEntity()
 	{
-		assert(mLivingEntityCount < MAX_ENTITIES && "Too many entities in existence.");
+		assert(livingEntityCount < MAX_ENTITIES && "Too many entities in existence.");
 
 		// Get ID from the front of the queue of available IDs
-		Entity id = mAvailableEntities.front();
-		mAvailableEntities.pop();
-		++mLivingEntityCount;
+		Entity id = availableEntities.front();
+		availableEntities.pop();
+		++livingEntityCount;
 
 		return id;
 	}
@@ -27,11 +27,11 @@ public:
 		assert(entity < MAX_ENTITIES && "Entity out of range.");
 
 		// Invalidate the destroyed entity's signature
-		mSignatures[entity].reset();
+		signatures[entity].reset();
 
 		// Put the destroyed ID at the back of the queue
-		mAvailableEntities.push(entity);
-		--mLivingEntityCount;
+		availableEntities.push(entity);
+		--livingEntityCount;
 	}
 
 	void SetSignature(Entity entity, Signature signature)
@@ -39,7 +39,7 @@ public:
 		assert(entity < MAX_ENTITIES && "Entity out of range.");
 
 		// Put this entity's signature into the array
-		mSignatures[entity] = signature;
+		signatures[entity] = signature;
 	}
 
 	Signature GetSignature(Entity entity)
@@ -47,16 +47,16 @@ public:
 		assert(entity < MAX_ENTITIES && "Entity out of range.");
 
 		// Get this entity's signature from the array
-		return mSignatures[entity];
+		return signatures[entity];
 	}
 
 private:
 	// Queue of unused entity IDs
-	std::queue<Entity> mAvailableEntities{};
+	std::queue<Entity> availableEntities{};
 
 	// Array of signatures where the index corresponds to the entity ID
-	std::array<Signature, MAX_ENTITIES> mSignatures{};
+	std::array<Signature, MAX_ENTITIES> signatures{};
 
 	// Total living entities - used to keep limits on how many exist
-	uint32_t mLivingEntityCount{};
+	uint32_t livingEntityCount{};
 };
