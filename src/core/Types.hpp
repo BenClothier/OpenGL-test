3b6b9a4d0fa7eae96a930b3427ed3a109565f1fa
Ben Clothier <ben.clothier8@icloud.com>
2022-06-04 11:09:15 +0100

    Create component-type ID

--- src/core/Types.hpp
+++ src/core/Types.hpp
@@ -4,3 +4,6 @@
 // Entity Component System
 using Entity = std::uint32_t; // Each entity is defined by a simple ID
 const Entity MAX_ENTITIES = 5000;
+
+using ComponentType = std::uint8_t; // Each component has a simple ID assigned to it
+const ComponentType MAX_COMPONENTS = 32;
