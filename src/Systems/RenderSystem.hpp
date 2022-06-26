20284467e913d082754f6235270677e6d7d457ef
Ben Clothier <ben.clothier8@icloud.com>
2022-06-26 16:19:30 +0100

    Add all opengl code to the rendering system script

--- src/Systems/RenderSystem.hpp
+++ src/Systems/RenderSystem.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 #include "Core/System.hpp"
 
 class RenderSystem : public System
@@ -7,4 +11,9 @@ class RenderSystem : public System
 public:
 	void Init();
 	void Update(float dt);
+	void Finish();
+private:
+	GLFWwindow* window;
+    unsigned int VBO, VAO;
+	unsigned int shaderProgram;
 };
