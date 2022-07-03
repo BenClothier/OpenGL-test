dbad501fe43c87daf3e7262bdccc059feb22a3e6
Ben Clothier <ben.clothier8@icloud.com>
2022-07-03 14:17:20 +0100

    Create texture class

---
+++ src/core/Texture.hpp
@@ -0,0 +1,37 @@
+#include "../STB/stb_image.h"
+
+#include <glad/glad.h>
+
+class Texture
+{
+public:
+    int width, height, nrChannels;
+    unsigned int texture;
+
+    Texture()
+    {
+    }
+
+    Texture(const char *imagePath)
+    {
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        data = stbi_load(imagePath, &width, &height, &nrChannels, 0);
+
+		assert(data && "Failed to load image.");
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(data);
+    }
+
+private:
+    unsigned char *data;
+};
